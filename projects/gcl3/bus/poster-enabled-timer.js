0b33971e9221d1b0cecb44e88b1baf9b7a893715
oudream <oudream@126.com>
2019-11-18 19:34:31 +0800

    up

---
+++ projects/gcl3/bus/poster-enabled-timer.js
@@ -0,0 +1,45 @@
+const puppeteer = require('puppeteer');
+
+function delay(time) {
+    return new Promise(function(resolve) {
+        setTimeout(resolve, time)
+    });
+}
+
+(async () => {
+    // const browser = await puppeteer.launch({
+    //     executablePath: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
+    //     dumpio: true
+    // });
+    const browser = await puppeteer.launch({
+        headless: true,
+        args: ['--no-sandbox', '--headless', '--disable-gpu', '--disable-dev-shm-usage']
+    });
+    // const browser = await puppeteer.launch()
+    // const browser = await puppeteer.launch({
+    //     headless: true,
+    //     executablePath: '/usr/bin/chromium-browser',
+    //     args: ['--no-sandbox', '--headless', '--disable-gpu', '--disable-dev-shm-usage']
+    // })
+    const page = await browser.newPage()
+
+    const navigationPromise = page.waitForNavigation()
+
+    await page.goto('http://122.51.12.151:2281/bus/poster.html')
+
+    await page.setViewport({width: 1280, height: 698})
+
+    await navigationPromise
+
+    await page.waitForSelector('body #uiMidScopeAddBn')
+    await page.click('body #uiMidScopeAddBn')
+
+    await page.waitForSelector('body #uiTimeOutEnabled')
+    await page.click('body #uiTimeOutEnabled')
+
+    console.log('before waiting');
+    await delay(1000 * 60 * 60);
+    console.log('after waiting');
+
+    await browser.close()
+})();
